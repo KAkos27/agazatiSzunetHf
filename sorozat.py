ba62a5216d8ede6b10cb3d2779655b99b0461cd8
KAkos27 <akoskosztolanyi@gmail.com>
2023-12-27 20:29:52 +0100

    masodikfeladatkesz

--- sorozat.py
+++ sorozat.py
@@ -1,33 +1,36 @@
 import random
 import math
 
-def masodik_feladat():
-    szam_lista_szoveg:str=""
-    szam_lista:str=[]
 
-    for i in range(0,16,1):
-        szam:int = math.floor(random.random()*591-90)
-        szam_lista_szoveg+=str(szam)
+def masodik_feladat():
+    szam_lista_szoveg: str = ""
+    szam_lista: str = []
+
+    for i in range(0, 16, 1):
+        szam: int = math.floor(random.random() * 591 - 90)
+        szam_lista_szoveg += str(szam)
         if i != 15:
-            szam_lista_szoveg+="*"
-    
+            szam_lista_szoveg += "*"
+
     szam_lista.append(szam_lista_szoveg)
     print(szam_lista)
     return szam_lista
-    
+
+
 def oszthatok_szama(lista):
     lista = lista[0].split("*")
-    oszthatok:int = 0
-
-    for i in range(0,len(lista),1):
+    oszthatok: int = 0
+    for i in range(0, len(lista), 1):
         if int(lista[i]) % 10 == 0:
             oszthatok += 1
     return oszthatok
 
+
 def konzolra_ir(oszthatok):
     print(f"Tízzel osztható számok száma: {oszthatok}")
 
+
 def fajl_ir(oszthatok):
-    fajl = open("kimutatas.txt","w",encoding="utf-8")
+    fajl = open("kimutatas.txt", "w", encoding="utf-8")
     fajl.write(f"Tízzel osztható számok száma: {oszthatok}")
     fajl.close()
